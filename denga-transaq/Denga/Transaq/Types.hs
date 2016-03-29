7b4c4e8aafe307484732351c0a0feaf42a6d3b82
Leonid Vlasenkov <leo.vlasenkov@gmail.com>
2016-03-29 14:38:11 +0300

    edited dependencies, wrote types

--- denga-transaq/Denga/Transaq/Types.hs
+++ denga-transaq/Denga/Transaq/Types.hs
@@ -7,14 +7,7 @@
 
 -- | 
 
-module Denga.Transaq.Types
-  ( Transaq (..)
-  , ServiceInfo (..)
-  , Settings (..)
-  , Connection (..)
-  , XML
-  , BString
-  ) where
+module Denga.Transaq.Types where
 
 
 
@@ -22,7 +15,9 @@ import           Control.Monad.State
 import           Control.Exception
 import           Control.Lens
 import qualified Data.ByteString as B
+import           Data.ByteString.Lex.Integral
 import qualified Data.Map as Map
+import           Data.Maybe
 import           Data.Default
 import           Data.Typeable (Typeable)
 import           Text.XML.Expat.Tree
@@ -34,11 +29,18 @@ type BString = B.ByteString
 type XML = Node BString BString
 type Transaq = StateT TransaqState IO
 
+uIntToBS :: Int -> BString
+uIntToBS x = case packDecimal x of
+    Just s    -> s
+    otherwise -> throw $ TransaqError "passed negative number to command"
 
+boolToBS :: Bool -> BString
+boolToBS x = if x then "true" else "false"
 
 data TransaqError
     = TransaqXMLParseError String
     | TransaqCommandResultError String
+    | TransaqError String
     deriving (Show, Typeable)
 
 instance Exception TransaqError
@@ -89,14 +91,14 @@ data Connection = Connection
     { login           :: !BString
     , password        :: !BString
     , host            :: !BString
-    , port            :: !BString
+    , port            :: !Int
     , autopos         :: !Maybe Bool
     , micex_registers :: !Maybe Bool
     , milliseconds    :: !Maybe Bool
     , utc_time        :: !Maybe Bool
     , proxy           :: !Maybe Proxy
-    , rqdelay         :: !Maybe
-    , session_timeout :: !Maybe 
+    , rqdelay         :: !Maybe Int
+    , session_timeout :: !Maybe Int
     , request_timeout :: !Maybe Int
     , push_u_limits   :: !Maybe Int
     , push_pos_equity :: !Maybe Int
@@ -107,7 +109,7 @@ instance Default Connection where
         { login           = ""
         , password        = ""
         , host            = ""
-        , port            = ""
+        , port            = 0
         , autopos         = Nothing
         , micex_registers = Nothing
         , milliseconds    = Nothing
@@ -120,7 +122,25 @@ instance Default Connection where
         , push_pos_equity = Nothing
         }
 
-
+connectionToXML :: Connection -> XML
+connectionToXML c = Element "command" [("id", "connect")]
+    [ Element "login" [] [Text $ login c]
+    , Element "password" [] [Text $ password c]
+    , Element "host" [] [Text $ host c]
+    , Element "port" [] [Text $ uIntToBS $ port c]
+    , Element "autopos" [] [Text $ boolToBS $ autopos c]
+    ] ++ $ catMaybes
+        [ fmap (\x -> Element "autopos" [] [Text $ boolToBS x]) $ autopos c
+        , fmap (\x -> Element "micex_registers" [] [Text $ boolToBS x]) $ micex_registers c
+        , fmap (\x -> Element "milliseconds" [] [Text $ boolToBS x]) $ milliseconds c
+        , fmap (\x -> Element "utc_time" [] [Text $ boolToBS x]) $ utc_time c
+        , fmap (\x -> proxyToXML x) $ proxy c
+        , fmap (\x -> Element "rqdelay" [] [Text $ uIntToBS x]) $ rqdelay c
+        , fmap (\x -> Element "session_timeout" [] [Text $ uIntToBS x]) $ session_timeout c
+        , fmap (\x -> Element "request_timeout" [] [Text $ uIntToBS x]) $ request_timeout c
+        , fmap (\x -> Element "push_u_limits" [] [Text $ uIntToBS x]) $ push_u_limits c
+        , fmap (\x -> Element "push_pos_equity" [] [Text $ uIntToBS x]) $ push_pos_equity c
+        ]
 
 data Proxy = Proxy
     { proxyType     :: !Maybe BString
@@ -139,5 +159,13 @@ instance Default Proxy where
         , proxyPassword = Nothing
         }
 
-
+proxyToXML :: Proxy -> XML
+proxyToXML p = Element "proxy" attrs []
+    where attrs = catMaybes
+        [ fmap (\x -> ("type", x)) $ proxyType p
+        , fmap (\x -> ("addr", x)) $ proxyAddr p
+        , fmap (\x -> ("port", x)) $ proxyPort p
+        , fmap (\x -> ("login", x)) $ proxyLogin p
+        , fmap (\x -> ("password", x)) $ proxyPassword p
+        ]
 
